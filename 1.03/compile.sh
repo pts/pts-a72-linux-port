41ad04d3c1f6e3ee8b10e872359b44e2a689c615
Peter Szabo <pts@fazekas.hu>
2024-03-29 23:09:59 +0100

    added bootstrap script 1.03/compile.sh and expected golden *.dis output files

---
+++ 1.03/compile.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# by pts@fazekas.hu at Fri Mar 29 22:47:45 CET 2024
+set -ex
+test "${0%/*}" = "$0" || cd "${0%/*}"
+test -f ../tools/nasm-0.98.39
+test -f ../tools/kvikdos
+export PATH="../tools:$PATH"
+
+kvikdos a72.com /a a72.asm a72p0.com
+cmp a72.com a72p0.com
+kvikdos a72.com /d a72.com a72p0.dis
+diff a72.dis a72p0.dis
+
+kvikdos a72.com /a a72_8087.asm a728p0.com
+cmp a72_8087.com a728p0.com
+kvikdos a72.com /d a72_8087.com a728p0.dis
+diff a72_8087.dis a728p0.dis
+
+: "$0" OK.
